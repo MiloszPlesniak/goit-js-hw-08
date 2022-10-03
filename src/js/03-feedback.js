4952141b5dade40e5d04c254bbc0b55462805b4b
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-10-04 00:10:17 +0200

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,33 @@
 var throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
-const inputEmail = form.firstElementChild.;
-const inputMessege = inputEmail.nextElementSibling;
+const inputEmail = form.firstElementChild.firstElementChild
+const inputMessege = form.children[1].firstElementChild
 
+let data = {
+    Email: "",
+    Messeage: ""
+}
+
+form.addEventListener('input', throttle(() => {
+    data.Email = inputEmail.value;
+    data.Messeage = inputMessege.value
+    localStorage.setItem("feedback-form-state",JSON.stringify(data))
+    
+
+}, 500))
+let actualyStorage = JSON.parse(localStorage.getItem("feedback-form-state"))
+
+if (actualyStorage != null ) {
+    inputEmail.value = actualyStorage.Email;
+    inputMessege.value = actualyStorage.Messeage;
+}
 
 
 form.addEventListener('submit', (event) => {
-    // event.preventDefault()
-    let items = {
-        Email: event.target[0].value,
-        Message: event.target[1].value
-    }
-    localStorage.setItem("feedback-form-state", JSON.stringify(items))
+    event.preventDefault()
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")))
+    localStorage.clear()
+    form.reset()
     
 })
-inputEmail.textContent = ""
-console.log(inputEmail.target)
