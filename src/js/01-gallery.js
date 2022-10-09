3775dbcf6b2d72f5a800356047c878d8650f0651
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-10-09 20:23:51 +0200

    d

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-import "simplelightbox/dist/simple-lightbox.min.css";
-import SimpleLightbox from "simplelightbox";
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// // import "simplelightbox/dist/simple-lightbox.min.css";
+// // import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
-const gallery = document.querySelector('.gallery');
+// console.log(galleryItems);
+// const gallery = document.querySelector('.gallery');
 
-for (let i = 0; i < galleryItems.length; i++) {
-    const newLink = document.createElement('a');
-    newLink.classList.add('gallery__item')
-    newLink.setAttribute('href', galleryItems[i].original)
-    newLink.setAttribute('onclick', 'return false')
-    gallery.append(newLink)
-    const newImg = document.createElement('img');
-    newImg.classList.add('gallery__image');
-    newImg.setAttribute('src', galleryItems[i].preview)
-    newImg.setAttribute('alt', galleryItems[i].description)
-    newLink.append(newImg)
-}
+// for (let i = 0; i < galleryItems.length; i++) {
+//     const newLink = document.createElement('a');
+//     newLink.classList.add('gallery__item')
+//     newLink.setAttribute('href', galleryItems[i].original)
+//     newLink.setAttribute('onclick', 'return false')
+//     gallery.append(newLink)
+//     const newImg = document.createElement('img');
+//     newImg.classList.add('gallery__image');
+//     newImg.setAttribute('src', galleryItems[i].preview)
+//     newImg.setAttribute('alt', galleryItems[i].description)
+//     newLink.append(newImg)
+// }
 
 
-let secGallery = new SimpleLightbox('.gallery a');
-secGallery.on('show.simplelightbox');
+// let secGallery = new SimpleLightbox('.gallery a');
+// secGallery.on('show.simplelightbox');
 
-console.log(secGallery.options)
+// console.log(secGallery.options)
 
-secGallery.options.captionsData = "alt"
-secGallery.options.captionDelay = 250
+// secGallery.options.captionsData = "alt"
+// secGallery.options.captionDelay = 250
+console.log(date.now(
+    
+))
